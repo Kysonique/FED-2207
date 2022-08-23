9d0769dcd49907a17280938c5d01338878227c39
Kysonique <Kyras.bradley@gmail.com>
2022-08-23 18:35:43 -0400

    finishing assignments

--- Assignments/03 Assignment 5 JS Find the remainder/main.js
+++ Assignments/03 Assignment 5 JS Find the remainder/main.js
@@ -1,9 +1,17 @@
-function factorial(k) {
-    if (k==0) {
-        return false;
-    } else {
-        return (k-11);
-    }
+// declartion an assgning variables
+var largeNumber; 
+    largeNumber = 3467;
+
+var smallNumber;
+    smallNumber = 325;
+
+console.log("What is 3467 divided by 325 (using subraction)");
+
+// large number minus the smaller number until can't subract anymore
+
+function findTheRemainder() {
+    return 3467-325;
 }
 
-console.log(factorial)
+console.log("The first answer is:" + findTheRemainder())
+
