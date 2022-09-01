2da7679f4a14d2567ad742ad070f79147e414267
Kysonique <Kyras.bradley@gmail.com>
2022-09-01 08:18:03 -0400

    saving

--- Assignments/04 Assignment 7 CRUD/main.js
+++ Assignments/04 Assignment 7 CRUD/main.js
@@ -1,17 +1,21 @@
+
+console.log('JS connected')
+
 $(document).ready(function(){
 
     $.ajax({
       Type: "GET",
       datatype: "json",
-      URL: "./data.json",
-      Sucess :function (data, status, xhr){
-        console.log(data, users)
+      src: "C:\Users\kyras\OneDrive\Desktop\FED-2207\Assignments\04 Assignment 7 CRUD\data.json",
+     
+      Sucess : function (data, status, xhr){
+        console.log(data)
       }
-    })
+    });
 
 
 
     $(".newbutton").click(function(){
       $(".form1").toggle();
     });
-  });
+});
