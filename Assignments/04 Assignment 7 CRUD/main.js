f453706a449c5fb59d181cadd76874d6a737f8bd
Kysonique <Kyras.bradley@gmail.com>
2022-09-02 09:24:33 -0400

    working on twtclone

--- Assignments/04 Assignment 7 CRUD/main.js
+++ Assignments/04 Assignment 7 CRUD/main.js
@@ -6,11 +6,13 @@ $(document).ready(function(){
     $.ajax({
       Type: "GET",
       datatype: "json",
-      src: "C:\Users\kyras\OneDrive\Desktop\FED-2207\Assignments\04 Assignment 7 CRUD\data.json",
-     
+      url: "./Assignments\04 Assignment 7 CRUD\data.json",
+    
       Sucess : function (data, status, xhr){
-        console.log(data)
+        console.log('data')
+
       }
+      
     });
 
 
