10afdd9462297df52c0389dc559e6a047e246cee
Kysonique <Kyras.bradley@gmail.com>
2022-09-04 21:31:16 -0400

    saving

--- Assignments/02 Assignment 4 html/mobile js/Mobile.js
+++ Assignments/02 Assignment 4 html/mobile js/Mobile.js
@@ -1,12 +1,11 @@
-
-console.log('JS connected')
+console.log("js connected")
 
 $(document).ready(function(){
 
     $.ajax({
       Type: "GET",
       datatype: "json",
-      url: "data.json",
+      url: "mobile.json",
     
       Sucess : function (data, status, xhr){
         console.log('data')
